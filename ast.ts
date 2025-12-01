90c92a25e8a6066bd94edbe3b81b8f42aa024bfa
Revanth686 <revanthperumalla3503@gmail.com>
2025-12-01 20:11:29 +0530

    adding types that will be used in building ast

---
+++ ast.ts
@@ -0,0 +1,44 @@
+// -----------------------------------
+// ------------- AST (Abstract Syntax Tree) Types ---------
+// ------ Defines the stucture of our language's AST -----
+// -----------------------------------
+
+export type NodeType =
+  | "Program"
+  | "NumericLiteral"
+  | "BinaryExpr"
+  | "Identifier";
+
+//NOTE: currently statement doesn't return anything
+//but in some REPL's statements evaluate to *undefined*
+//ex: Let x=119 --statement
+//x=119 --assignment expression in c, cpp, java -evaluate to 119  but in python its a statement
+
+export interface Stmt {
+  type: NodeType;
+}
+
+export interface Program extends Stmt {
+  type: "Program";
+  body: Stmt[];
+}
+
+export interface Expr extends Stmt {}
+
+export interface BinaryExpr extends Expr {
+  //foo + bar
+  kind: "BinaryExpr";
+  left: Expr;
+  right: Expr;
+  operator: string;
+}
+
+export interface Identifier extends Expr {
+  kind: "Identifier";
+  symbol: string;
+}
+
+export interface NumericLiteral extends Expr {
+  kind: "NumericLiteral";
+  value: Number;
+}
